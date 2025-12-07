ee7849ea8eb49ac0f4256d6c28d29932c5e9b9d9
Matthew Jones <jones.allen.matthew@gmail.com>
2025-12-07 15:32:56 -0500

    Fixed formatting and finalized read_file

--- read_file.c
+++ read_file.c
@@ -27,67 +27,60 @@ int main(int argc, char *argv[]) {
     printf("Opened disk image: %s\n", argv[1]);
 #endif
 
-	// store input info
+	// Store input info
 	char * extractFile = argv[2];
 	FILE * storeFile = fopen(argv[3], "wb+");
 	
-
     // Read superblock
     superblock_t sb;
     fread(&sb, sizeof(superblock_t), 1, fp);
     
     // Go to directories
-	uint32_t offset = sizeof(superblock_t); //offset
-	int usedDir = sb.total_direntries - sb.available_direntries; //total entries
-	fseek(fp, offset, SEEK_SET); // set dir location
+	uint32_t offset = sizeof(superblock_t); // Offset
+	int usedDir = sb.total_direntries - sb.available_direntries; // Total entries
+	fseek(fp, offset, SEEK_SET); // Set dir location
 	
-	//find file
+	// Find file
 	int start = -1;
 	struct direntry entry;
 	for(int dir = 0; dir < usedDir; dir ++){
-		//store file entry
+		// Store file entry
 		fread(&entry, sizeof(entry), 1, fp);
         
-        //if names are the same
+        // If names are the same
         if(!strcmp(entry.filename, extractFile)){
 			start = entry.starting_block;
 			break;
 		} 
 	}
 	
-	//check if file found
+	// Check if the file found
 	if(start == -1){
 		printf("ERROR: File doesn't exist");
 	} else {
-		//find offset and start location
+		// Find offset and start location
 		uint32_t diroffset = sizeof(superblock_t) + sizeof(struct direntry) * sb.total_direntries + sb.bytes_per_block * start; //offset
-		fseek(fp, diroffset, SEEK_SET); // set dir location
+		fseek(fp, diroffset, SEEK_SET); // Set dir location
 		
-		printf("offset %d", diroffset);
-		fflush(stdout);
-		
-		//loop
 		struct fileblock data;
 		data.data = malloc(sb.bytes_per_block - 3);
 		int currSize = entry.file_size;
-		do {
-			//fread(&data, sizeof(data), 1, fp);
-			
-			//read in file info 
+		
+		do {			
+			// Read in file info 
 			fread(&data.is_busy, 1, 1, fp);
 
-			// 2. Read the data bytes
+			// Read the data bytes
 			fread(data.data, sb.bytes_per_block - 3, 1, fp);
 
-			// 3. Read next_block
+			// Read next_block
 			fread(&data.next_block, sizeof(uint16_t), 1, fp);
 			
-			//write to file
+			// Write to file
 			fwrite(data.data, sb.bytes_per_block - 3, 1, storeFile);
 			currSize -= (sb.bytes_per_block - 3);
 		} while (currSize >= 0);
 	}
-	
 
     fclose(fp);
     fclose(storeFile);
