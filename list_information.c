f1f4a15079fab0789cc6d1293aaba53a88664fe4
SunPikl <picklebreadz@gmail.com>
2025-12-07 13:34:27 -0500

    list info

--- list_information.c
+++ list_information.c
@@ -1,3 +1,11 @@
+/**
+ * Lists the information of a QFS disk file
+ * 
+ * Authors: Morgan Montz & Matthew Jones
+ * 
+ **/
+
+
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
@@ -30,11 +38,27 @@ int main(int argc, char *argv[]) {
     printf("Amount of Free Blocks: %d\n", sb.available_blocks);
     printf("Total Directory Entries: %d\n", sb.total_direntries);
     printf("Amount of Free Directory Entries: %d\n", sb.available_direntries);
+    printf("\n-----------------------------\n");
 	
-	//offset?
+	//offset
+	uint32_t offset = sizeof(superblock_t);
 	
+	//tot entries 
+	int usedDir = sb.total_direntries - sb.available_direntries;
+	
+	//set location
+	fseek(fp, offset, SEEK_SET);
 	
 	//all files in directory
+	for(int dir = 0; dir < usedDir; dir ++){
+		struct direntry entry;
+		fread(&entry, sizeof(entry), 1, fp);
+        
+        //print info
+        printf("File Name: %s\n", entry.filename);
+        printf("File Size: %d\n", entry.file_size);
+        printf("Start Block #: %d\n\n", entry.starting_block);
+	}
 
     fclose(fp);
     return 0;
