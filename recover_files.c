df188a8c4092476ff1ee912cb4c79b510b86c0a0
SunPikl <picklebreadz@gmail.com>
2025-12-07 17:27:46 -0500

    ... forgor seek

--- recover_files.c
+++ recover_files.c
@@ -20,7 +20,51 @@ int main(int argc, char *argv[]) {
     printf("Opened disk image: %s\n", argv[1]);
 #endif
 
-    // TODO
+    // open super
+    superblock_t sb;
+    fread(&sb, sizeof(superblock_t), 1, fp);
+    
+    // offset
+    uint32_t diroffset = sizeof(superblock_t); //offset
+    uint32_t dataoffset = sizeof(superblock_t) + sizeof(struct direntry) * sb.total_direntries ; //offset
+    fseek(fp, dataoffset, SEEK_SET); // set dir location
+    
+    // loop through blocks
+	struct fileblock data;
+	data.data = malloc(sb.bytes_per_block - 3);
+	int currFile = 1;
+	FILE * newFile;
+	for(int block = 0; block <  sb.total_blocks; block ++){			
+		// get block data 
+		fread(&data.is_busy, 1, 1, fp); // is block busy		
+		fread(data.data, sb.bytes_per_block - 3, 1, fp); // read the data bytes
+		fread(&data.next_block, sizeof(uint16_t), 1, fp); // read next_block
+		
+		//if data not free aka has data
+		if(!data.is_busy){
+			//if start of file
+			if(data.data[0] == 0xFF && data.data[1] == 0xD8){
+				//make dir 
+				
+				//
+			} 
+			
+			//new file
+			//newFile = fopen(
+			
+			//update dir info
+			
+		}
+		
+		//write to file
+		//fwrite(data.data, sb.bytes_per_block - 3, 1, storeFile);
+		//currSize -= (sb.bytes_per_block - 3);
+	} 
+    // if start 0xFF 0xD8
+	//  set dir info
+    //  loop through blocks until no next OR end in 0xFF 0xD9
+	// 		store data to uncovered file
+    //      if end, count num bytes, set dir size
 
     fclose(fp);
     return 0;
