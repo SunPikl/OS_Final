30239eb973583313f4f539037e13b9d6c4e3bafb
SunPikl <picklebreadz@gmail.com>
2025-12-11 19:06:52 -0500

    curr stoof

--- delete_file.c
+++ delete_file.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include <stdlib.h>
 #include "qfs.h"
 
 int main(int argc, char *argv[]) {
@@ -19,8 +20,83 @@ int main(int argc, char *argv[]) {
     printf("Opened disk image: %s\n", argv[1]);
 #endif
 
-    // TODO
+	// Store input info
+	char * removeFile = argv[2];
+	
+    // Read superblock
+    superblock_t sb;
+    fread(&sb, sizeof(superblock_t), 1, fp);
+    
+    // Go to directories
+	uint32_t offset = sizeof(superblock_t); // Offset
+	int usedDir = sb.total_direntries - sb.available_direntries; // Total entries
+	fseek(fp, offset, SEEK_SET); // Set dir location
+	
+	// Find file
+	int start = -1;
+	struct direntry entry;
+	for(int dir = 0; dir < usedDir; dir ++){
+		// Store file entry
+		fread(&entry, sizeof(entry), 1, fp);
+        
+        // If names are the same
+        if(!strcmp(entry.filename, removeFile)){
+			start = entry.starting_block;
+			
+			// Clean Entry
+			memset(entry.filename, 0, sizeof(entry.filename));        
+			entry.permissions = 0;          
+			entry.owner_id = 0;            
+			entry.group_id = 0;             
+			entry.starting_block = 0;    
+			entry.file_size = 0;  
+			
+			// Replace Entry
+			fseek(fp, sizeof(superblock_t) + sizeof(struct direntry) * dir, SEEK_SET);
+			fwrite(&entry, sizeof(struct direntry), 1, fp);
+			
+			break;
+		} 
+	}
+	          
+	
+	// Check if the file found
+	if(start == -1){
+		printf("ERROR: File doesn't exist");
+	} else {
+		// Find offset and start location
+		uint32_t diroffset = sizeof(superblock_t) + sizeof(struct direntry) * sb.total_direntries + sb.bytes_per_block * start; //offset
+		fseek(fp, diroffset, SEEK_SET); // Set dir location
+		
+		struct fileblock data;
+		data.data = malloc(sb.bytes_per_block - 3);
+		int currSize = entry.file_size;
+		
+		do {			
+			// Read in file info 
+			fread(&data.is_busy, 1, 1, fp);
 
+			// Read the data bytes
+			fread(data.data, sb.bytes_per_block - 3, 1, fp);
+
+			// Read next_block
+			fread(&data.next_block, sizeof(uint16_t), 1, fp);
+			
+			// Write to file
+			//fwrite(data.data, sb.bytes_per_block - 3, 1, storeFile);
+			currSize -= (sb.bytes_per_block - 3);
+            fseek(fp, sizeof(superblock_t) + sizeof(struct direntry) * sb.total_direntries + sb.bytes_per_block * data.next_block, SEEK_SET);
+		} while (currSize >= 0);
+	}
+	
+	// Fix superblock
+    superblock_t tempsb = sb;
+    //tempsb.available_blocks = sb.available_blocks + numBlocks;
+    tempsb.available_direntries = sb.available_direntries + 1;
+    
+    fseek(fp, 0, SEEK_SET);
+    fwrite(&tempsb, sizeof(superblock_t), 1, fp);
+    
     fclose(fp);
     return 0;
 }
